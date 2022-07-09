179de55c9bb7a79e3336c5194633aec3b76ebfc9
yermiah <aliyermiah6@gmail.com>
2022-07-09 03:27:25 -0400

    secondcommit

---
+++ src/main/java/mavenproject/Sample.java
@@ -0,0 +1,13 @@
+package mavenproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class Sample
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
